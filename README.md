4e2e5ce67fb25ab20f9ba837909605146fb99a91
Alex J V <alexjv89@gmail.com>
2023-11-24 16:43:27 +0530

    bulkrun logic added

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # bulkrun
 bulkrun commands from terminal
+
+## Install 
+```
+npm install  bulkrun --save-dev
+```
+
+## Run 
+```
+node node_modules/@alexjv89/bulkrun --file="temp/bulk_run/movement_FY2023-24.txt"
+```
+
+## Options
+- `--file` - specify file containing commands to run
+- `--limit` - execute a maximum of this many commands from the file
+- `--retry` - retry failed commands
+- `--sleep` - sleep time in seconds between execution of each line
+
+## Others
+github repo - https://github.com/alexjv89/bulkrun
+npm - https://www.npmjs.com/package/@alexjv89/bulkrun
--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-console.log('bulk run');
+/* ============================================CONFIGURATIONS============================================ */
+var commandLineArgs = require('command-line-args');
+const argVariables = [
+	{ name: 'limit', type: Number }, // Max no of command to run 
+	{ name: 'sleep', type: Number }, // sleep between command executions in seconds
+	{ name: 'file', type: String }, // file containing commands to execute
+	{ name: 'retry', type: Boolean }, // to retry failed jobs
+	
+];
+const cli_args = commandLineArgs(argVariables);
+
+if(!cli_args.file)
+	return console.log('please specify file containing commands to execute');
+/* ============================================CONFIGURATIONS============================================ */
+
+
+var fs = require('fs');
+var shell = require('shelljs');
+// ----------------------------- Config -----------------------------
+var limit = 50; 
+var sleep = 1; 
+// --------------------------- Config END ---------------------------
+
+var count = 0;
+
+
+console.log('retry = '+cli_args.retry);
+
+
+var config = fs.readFileSync(cli_args.file, 'utf-8');
+var lines = config.split(/\r?\n/);
+var updated_lines = [];
+
+lines.forEach(line =>  {
+	if(count>=limit){
+		updated_lines.push(line);
+		return;
+	}
+
+	if(line.indexOf('node run_local')>-1){
+		var command = line.split('#')[0].trim();
+		var status = line.split('#')[1].trim();
+		if(status=='run' || (cli_args.retry && status=='failed')){
+			// console.log('');
+			console.log('\n\n\n===============================');
+			console.log(`>>>>> Line from file: ${command} - ${status}`);
+			shell.exec(`sleep ${sleep}`);
+			var a = shell.exec(command);
+			if(a.stderr)
+				status='failed';
+			else 
+				status='done';
+			updated_lines.push(`${command} # ${status}`);
+			count++;
+		}else{
+			updated_lines.push(line);
+		}
+	}else{
+		updated_lines.push(line);
+	}
+});
+
+fs.writeFileSync(cli_args.file,updated_lines.join('\r\n'));
